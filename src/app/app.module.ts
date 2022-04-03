f260914178e2067750778ae872bd25e9d4c081b6
heshanu <heshanu97@gmail.com>
2022-04-03 03:55:14 -0700

    added c server-element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CockpitComponent } from './compoent/cockpit/cockpit.component';
+import { ServerElementComponent } from './compoent/server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CockpitComponent
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
