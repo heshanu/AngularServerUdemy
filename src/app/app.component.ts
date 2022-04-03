a103958472f46a0ca315ed10f7a81b2106743a51
heshanu <heshanu97@gmail.com>
2022-04-03 03:13:33 -0700

    bug solved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NgServerUdemy';
+  serverElements = [];
+  newServerName = '';
+  newServerContent = '';
+  servers:any= '';
+
+  onAddServer() {
+    this.servers.push("Another Servers!");
+    /*
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });*/
+  }
+
+  onAddBlueprint(id:number) {
+    const position=id+1;
+    this.servers.splice(position,1);
+  }
 }
